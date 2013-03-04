7d35e5acfa6506cb84409baf8e97e79b40c4db79
BenouKat <ben.goze@gmail.com>
2013-03-04 17:54:38 +0100

    [Build 1202] [Update]
    PackZone.cs terminé, next : SongZone
    
    Signed-off-by: BenouKat <ben.goze@gmail.com>

--- Assets/Script/Wheel/PackZone.cs
+++ Assets/Script/Wheel/PackZone.cs
@@ -9,15 +9,16 @@ public class PackZone : MonoBehaviour {
 	public GameObject miniCubePack;
 	public Camera camerapack;
 	
-	private Dictionary<string, GameObject> packs;
-	private Dictionary<GameObject, float> cubesPos;
+	private Dictionary<GameObject, string> packs;
 	
 	//PackList
 	private int numberPack;
 	private int nextnumberPack;
-	public float x10;
-	public float xm10;
-	public float y;
+	public Transform[] packpos; //pos base = new Vector3(0f, 13, 20f)
+	public Vector2 posYModule; //x : low, Y : high
+	public float speedPop;
+	public float decalYLabel;
+	public float decalXLabel;
 	public float wd;
 	public float ht;
 	public Rect posBackward;
@@ -26,163 +27,197 @@ public class PackZone : MonoBehaviour {
 	public float speedMove;
 	public float limite;
 	
-	private float decalFade;
-	private float decalFadeM;
 	private float fadeAlpha;
 	
 	private bool movinForward;
 	private bool movinBackward;
-	private bool movinForwardFast;
-	private bool movinBackwardFast;
+	
+	private bool activeModule;
 	// Use this for initialization
 	void Start () {
+		
+		activeModule = true;
 		numberPack = 0;
 		nextnumberPack = 0;
 		
 		
-		packs = new Dictionary<string, GameObject>();
-		cubesPos = new Dictionary<GameObject, float>();
+		packs = new Dictionary<GameObject, string>();
 		
-		var tempPos = new Dictionary<GameObject, float>();
-		var tempPack = new Dictionary<string, GameObject>();
+		var tempPos = new List<GameObject>();
+		var tempPack = new Dictionary<GameObject, string>();
 		var position = 0;
 		var thePackPosition = -1;
+		
 		foreach(var el in LoadManager.Instance.ListSong().Keys){
 			var thego = (GameObject) Instantiate(miniCubePack, new Vector3(0f, 13f, 20f), miniCubePack.transform.rotation);
 			if(LoadManager.Instance.ListTexture().ContainsKey(el)) thego.renderer.material.mainTexture = LoadManager.Instance.ListTexture()[el];
 			if(tempPack.ContainsKey(el)){ 
-				tempPack.Add(el + "(" + tempPack.Count + ")", thego);	
+				tempPack.Add(thego, el + "(" + tempPack.Count + ")");	
 				if(el + "(" + tempPack.Count + ")" == DataManager.Instance.packSelected) thePackPosition = position; 
 			}
 			else
 			{ 
-				tempPack.Add(el, thego); 
+				tempPack.Add(thego, el); 
 				if(el == DataManager.Instance.packSelected) thePackPosition = position;
 			}
-			tempPos.Add(thego, 0f);
 			position++;
 		}
 
 		if(DataManager.Instance.packSelected != "" && thePackPosition != -1){
 			for(int i=thePackPosition;i<tempPos.Count;i++){
-				cubesPos.Add(tempPos.ElementAt(i).Key, tempPos.ElementAt(i).Value);
 				packs.Add(tempPack.ElementAt(i).Key, tempPack.ElementAt(i).Value);
 			}
 			for(int i=0;i<thePackPosition;i++){
-				cubesPos.Add(tempPos.ElementAt(i).Key, tempPos.ElementAt(i).Value);
 				packs.Add(tempPack.ElementAt(i).Key, tempPack.ElementAt(i).Value);
 			}
 		}else{
 			for(int i=0;i<tempPos.Count;i++){
-				cubesPos.Add(tempPos.ElementAt(i).Key, tempPos.ElementAt(i).Value);
 				packs.Add(tempPack.ElementAt(i).Key, tempPack.ElementAt(i).Value);
 			}
 		}
 		
-		organiseCube();
+		organiseCube(0);
 		
 		activePack(packs.ElementAt(0).Key);
 		
-		decalFade = 0f;
-		decalFadeM = 0f;
 		fadeAlpha = 0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		//Move forward pack
+		if(movinForward){
+			
+			if(moveCube(Time.deltaTime/speedMove)){
+				movinForward = false;
+				fadeAlpha = 0.5f;
+				decreaseCubeF();
+				fadeAlpha = 0f;
+				numberPack = nextnumberPack;
+				organiseCube(numberPack);
+			}else{
+				decreaseCubeF();
+			}	
+		}
+		
+		//Move backward pack
+		if(movinBackward){
+			if(moveCube(Time.deltaTime/speedMove)){
+				movinBackward = false;	
+				decalFade = 0f;
+				decalFadeM = 0f;
+				fadeAlpha = 0.5f;
+				decreaseCubeB();
+				fadeAlpha = 0f;
+				numberPack = nextnumberPack;
+				organiseCube(numberPack);
+			}else{
+				decreaseCubeB();
+			}
+		}
 	}
 	
 	void OnGUI()
 	{
 		if(!LoadManager.Instance.isAllowedToSearch(search)){
-			var decal = ((Screen.width - wd*Screen.width)/2);
-			if(movinBackward) GUI.Label(new Rect((xm10*2f + decalFadeM)*Screen.width + decal, y*Screen.height, wd*Screen.width, ht*Screen.height), packs.ElementAt(PrevInt(numberPack, 2)).Key);
-			if(!movinForward) GUI.Label(new Rect((xm10 + decalFadeM)*Screen.width + decal, y*Screen.height, wd*Screen.width, ht*Screen.height), packs.ElementAt(PrevInt(numberPack, 1)).Key);
-			GUI.Label(new Rect(decalFade > 0 ? decalFade*Screen.width + decal : decalFadeM*Screen.width + decal  , y*Screen.height, wd*Screen.width, ht*Screen.height), packs.ElementAt(numberPack).Key);
-			if(!movinBackward) GUI.Label(new Rect((x10 + decalFade)*Screen.width + decal, y*Screen.height, wd*Screen.width, ht*Screen.height), packs.ElementAt(NextInt(numberPack, 1)).Key);
-			if(movinForward) GUI.Label(new Rect((x10*2f + decalFade)*Screen.width + decal, y*Screen.height, wd*Screen.width, ht*Screen.height), packs.ElementAt(NextInt(numberPack, 2)).Key);
-			
-		
-		
-			if(GUI.Button(new Rect(posBackward.x*Screen.width, posBackward.y*Screen.height, posBackward.width*Screen.width, posBackward.height*Screen.height),"","LBackward") && !movinBackward && !movinNormal && !movinOption){
-				nextnumberPack = PrevInt(numberPack, 1);
-				activePack(packs.ElementAt(nextnumberPack).Key);
-				movinBackward = true;
-				startnumber = 0;
-				currentstartnumber = 0;
-				camerapack.transform.position = new Vector3(0f, 0f, 0f);
-				cubeBase.transform.position = basePosCubeBase;
-			}
-			if(GUI.Button(new Rect(posBackward.x*Screen.width, posBackward.y*Screen.height + ecart*Screen.height, posBackward.width*Screen.width, posBackward.height*Screen.height),"","Backward") && !movinNormal && !movinOption){
-				nextnumberPack = PrevInt(numberPack, 3);
-				activePack(packs.ElementAt(nextnumberPack).Key);
-				movinBackwardFast = true;
-				startnumber = 0;
-				currentstartnumber = 0;
-				camerapack.transform.position = new Vector3(0f, 0f, 0f);
-				cubeBase.transform.position = basePosCubeBase;
-			}
-			
-			if(GUI.Button(new Rect(posForward.x*Screen.width, posForward.y*Screen.height, posForward.width*Screen.width, posForward.height*Screen.height),"","LForward") && !movinForward && !movinNormal && !movinOption)
+			for(int i=0; i < packs.Count; i++)
 			{
-				nextnumberPack = NextInt(numberPack, 1);
-				activePack(packs.ElementAt(nextnumberPack).Key);
-				movinForward = true;
-				startnumber = 0;
-				currentstartnumber = 0;
-				camerapack.transform.position = new Vector3(0f, 0f, 0f);
-				cubeBase.transform.position = basePosCubeBase;
+				if(packs.Count <= 7 || (i >= PrevInt(numberPack, 3) && i <= NextInt(numberPack, 3)))
+				{
+					var point2D = camerapack.WorldPointToScreen(packs.ElementAt(i).Key.transform.position);
+					GUI.Color = packs.ElementAt(i).Key.renderer.material.color;
+					GUI.Label(new Rect(point2D.x + (decalXLabel*Screen.width), point2D.y + (decalXLabel*Screen.width), wd*Screen.width, ht*Screen.height), packs.ElementAt(i).Value);
+					
+				}
 			}
-			if(GUI.Button(new Rect(posForward.x*Screen.width, posForward.y*Screen.height + ecart*Screen.height, posForward.width*Screen.width, posForward.height*Screen.height),"","Forward") && !movinNormal && !movinOption){
-				nextnumberPack = NextInt(numberPack, 3);
-				activePack(packs.ElementAt(nextnumberPack).Key);
-				movinForwardFast = true;
-				startnumber = 0;
-				camerapack.transform.position = new Vector3(0f, 0f, 0f);
-				cubeBase.transform.position = basePosCubeBase;
+			GUI.Color = new Color(1f, 1f, 1f, 1f);
+		
+			if(activeModule)
+			{
+				if(GUI.Button(new Rect(posBackward.x*Screen.width, posBackward.y*Screen.height, posBackward.width*Screen.width, posBackward.height*Screen.height),"","LBackward") && !movinBackward && !movinForward){
+					nextnumberPack = PrevInt(numberPack, 1);
+					organiseCube(numberPack);
+					movinBackward = true;
+				}
+				if(GUI.Button(new Rect(posBackward.x*Screen.width, posBackward.y*Screen.height + ecart*Screen.height, posBackward.width*Screen.width, posBackward.height*Screen.height),"","Backward") && !movinBackward && !movinForward ){
+					nextnumberPack = PrevInt(numberPack, 3);
+					organiseCube(nextnumberPack);
+					numberPack = nextnumberPack;
+				}
+				
+				if(GUI.Button(new Rect(posForward.x*Screen.width, posForward.y*Screen.height, posForward.width*Screen.width, posForward.height*Screen.height),"","LForward") && !movinBackward && !movinForward)
+				{
+					nextnumberPack = NextInt(numberPack, 1);
+					organiseCube(numberPack);
+					movinForward = true;
+				}
+				if(GUI.Button(new Rect(posForward.x*Screen.width, posForward.y*Screen.height + ecart*Screen.height, posForward.width*Screen.width, posForward.height*Screen.height),"","Forward") && !movinBackward && !movinForward){
+					nextnumberPack = NextInt(numberPack, 3);
+					organiseCube(nextnumberPack);
+					numberPack = nextnumberPack;
+				}
 			}
 		}
 	}
 	
 	
-	void organiseCube(){
-		cubesPos.ElementAt(0).Key.transform.position = new Vector3(0f, 13, 20f);
-		cubesPos[cubesPos.ElementAt(0).Key] = 0f;
-		cubesPos.ElementAt(0).Key.renderer.material.color = new Color(1f, 1f, 1f, 1f);
+	void organiseCube(int start){
 		
-		cubesPos.ElementAt(1).Key.transform.position = new Vector3(10f, 13, 20f);
-		cubesPos[cubesPos.ElementAt(1).Key] = 10f;
-		cubesPos.ElementAt(1).Key.renderer.material.color = new Color(1f, 1f, 1f, 0.5f);
+	
+		packs.ElementAt(start).Key.transform.position = packpos[2];
+		packs.ElementAt(start).Key.renderer.material.color = new Color(1f, 1f, 1f, 1f);
 		
-		cubesPos.ElementAt(2).Key.transform.position = new Vector3(20f, 13, 20f);
-		cubesPos[cubesPos.ElementAt(2).Key] = 20f;
-		cubesPos.ElementAt(2).Key.renderer.material.color = new Color(1f, 1f, 1f, 0f);
+		packs.ElementAt(NextInt(start, 1)).Key.transform.position = packpos[3];
+		packs.ElementAt(NextInt(start, 1)).Key.renderer.material.color = new Color(1f, 1f, 1f, 0.5f);
 		
-		cubesPos.ElementAt(cubesPos.Count - 1).Key.transform.position = new Vector3(-10f, 13, 20f);
-		cubesPos[cubesPos.ElementAt(cubesPos.Count - 1).Key] = -10f;
-		cubesPos.ElementAt(cubesPos.Count - 1).Key.renderer.material.color = new Color(1f, 1f, 1f, 0.5f);
+		packs.ElementAt(NextInt(start, 2)).Key.transform.position = packpos[4];
+		packs.ElementAt(NextInt(start, 2)).Key.renderer.material.color = new Color(1f, 1f, 1f, 0f);
 		
-		cubesPos.ElementAt(cubesPos.Count - 2).Key.transform.position = new Vector3(-20f, 13, 20f);
-		cubesPos[cubesPos.ElementAt(cubesPos.Count - 2).Key] = -20f;
-		cubesPos.ElementAt(cubesPos.Count - 2).Key.renderer.material.color = new Color(1f, 1f, 1f, 0f);
+		packs.ElementAt(PrevInt(start, 1)).Key.transform.position = packpos[1];
+		packs.ElementAt(PrevInt(start, 1)).Key.renderer.material.color = new Color(1f, 1f, 1f, 0.5f);
 		
-		for(int i=3; i<cubesPos.Count - 2;i++){
-			cubesPos.ElementAt(i).Key.renderer.material.color = new Color(1f, 1f, 1f, 0f);
+		packs.ElementAt(PrevInt(start, 2)).Key.transform.position = packpos[0];
+		packs.ElementAt(PrevInt(start, 2)).Key.renderer.material.color = new Color(1f, 1f, 1f, 0f);
+		
+		for(int i=0; i<packs.Count;i++){
+			var reverse = (i < PrevInt(numberPack, 2) || i > NextInt(numberPack, 2);
+			if((i < PrevInt(numberPack, 2) || i > NextInt(numberPack, 2))
+			{
+				packs.ElementAt(i).Key.transform.position = packpos[5]; //out
+				packs.ElementAt(i).Key.renderer.material.color = new Color(1f, 1f, 1f, 0f);
+			}
+			
 		}
+	}
+	
+	bool moveCube(float speed, float limit){
+		
+	
+		packs.ElementAt(PrevInt(nextnumberpack, 2)).transform.position = Vector3.Lerp(packs.ElementAt(PrevInt(nextnumberpack, 2)).transform.position, packpos[0], speed);
+		
+		packs.ElementAt(PrevInt(nextnumberpack, 1)).transform.position = Vector3.Lerp(packs.ElementAt(PrevInt(nextnumberpack, 1)).transform.position, packpos[1], speed);
+		
+		packs.ElementAt(nextnumberpack).transform.position = Vector3.Lerp(packs.ElementAt(nextnumberPack).transform.position, packpos[2], speed);
+		
+		packs.ElementAt(NextInt(nextnumberpack, 1)).transform.position = Vector3.Lerp(packs.ElementAt(NextInt(nextnumberpack, 1)).transform.position, packpos[3], speed);
+		
+		packs.ElementAt(NextInt(nextnumberpack, 2)).transform.position = Vector3.Lerp(packs.ElementAt(NextInt(nextnumberpack, 1)).transform.position, packpos[4], speed);
+		
+		return Math.Abs(packs.ElementAt(nextnumberpack).transform.position.x - packpos[2].x) <= limit;
 	}
 	
 	
 	void decreaseCubeF(){
 		
+		//Color
 		fadeAlpha = Mathf.Lerp(fadeAlpha, 0.5f, Time.deltaTime/speedMove);
-		cubesPos.ElementAt(numberPack).Key.renderer.material.color = new Color(1f, 1f, 1f, 1f - fadeAlpha);
+		packs.ElementAt(numberPack).Key.renderer.material.color = new Color(1f, 1f, 1f, 1f - fadeAlpha);
+		packs.ElementAt(NextInt(numberPack, 1)).Key.renderer.material.color = new Color(1f, 1f, 1f, 0.5f + fadeAlpha);
+		packs.ElementAt(NextInt(numberPack, 2)).Key.renderer.material.color = new Color(1f, 1f, 1f, 0f + fadeAlpha);
+		packs.ElementAt(PrevInt(numberPack, 1)).Key.renderer.material.color = new Color(1f, 1f, 1f, 0.5f - fadeAlpha);
 		
-		cubesPos.ElementAt(NextInt(numberPack, 1)).Key.renderer.material.color = new Color(1f, 1f, 1f, 0.5f + fadeAlpha);
 		
-		cubesPos.ElementAt(NextInt(numberPack, 2)).Key.renderer.material.color = new Color(1f, 1f, 1f, 0f + fadeAlpha);
 		
-		cubesPos.ElementAt(PrevInt(numberPack, 1)).Key.renderer.material.color = new Color(1f, 1f, 1f, 0.5f - fadeAlpha);
 	}
 	
 	
@@ -190,46 +225,13 @@ public class PackZone : MonoBehaviour {
 	void decreaseCubeB(){
 		
 		fadeAlpha = Mathf.Lerp(fadeAlpha, 0.5f, Time.deltaTime/speedMove);
-		cubesPos.ElementAt(numberPack).Key.renderer.material.color = new Color(1f, 1f, 1f, 1f - fadeAlpha);
-		
-		cubesPos.ElementAt(NextInt(numberPack, 1)).Key.renderer.material.color = new Color(1f, 1f, 1f, 0.5f - fadeAlpha);
-		
-		
-		cubesPos.ElementAt(PrevInt(numberPack, 1)).Key.renderer.material.color = new Color(1f, 1f, 1f, 0.5f + fadeAlpha);
-		
-		cubesPos.ElementAt(PrevInt(numberPack, 2)).Key.renderer.material.color = new Color(1f, 1f, 1f, 0f + fadeAlpha);
+		packs.ElementAt(numberPack).Key.renderer.material.color = new Color(1f, 1f, 1f, 1f - fadeAlpha);
+		packs.ElementAt(NextInt(numberPack, 1)).Key.renderer.material.color = new Color(1f, 1f, 1f, 0.5f - fadeAlpha);
+		packs.ElementAt(PrevInt(numberPack, 1)).Key.renderer.material.color = new Color(1f, 1f, 1f, 0.5f + fadeAlpha);
+		packs.ElementAt(PrevInt(numberPack, 2)).Key.renderer.material.color = new Color(1f, 1f, 1f, 0f + fadeAlpha);
 	}
 	
-	void fastDecreaseCubeF(){
-		
-		cubesPos.ElementAt(numberPack).Key.renderer.material.color = new Color(1f, 1f, 1f, 0f);
-		
-		cubesPos.ElementAt(NextInt(numberPack, 1)).Key.renderer.material.color = new Color(1f, 1f, 1f, 0f);
-		
-		cubesPos.ElementAt(NextInt(numberPack, 2)).Key.renderer.material.color = new Color(1f, 1f, 1f, 0.5f);
-		
-		cubesPos.ElementAt(NextInt(numberPack, 3)).Key.renderer.material.color = new Color(1f, 1f, 1f, 1f);
-		
-		cubesPos.ElementAt(NextInt(numberPack, 4)).Key.renderer.material.color = new Color(1f, 1f, 1f, 0.5f);
-		
-		cubesPos.ElementAt(PrevInt(numberPack, 1)).Key.renderer.material.color = new Color(1f, 1f, 1f, 0);
-	}
 	
-	void fastDecreaseCubeB(){
-		
-		cubesPos.ElementAt(numberPack).Key.renderer.material.color = new Color(1f, 1f, 1f, 0f);
-		
-		cubesPos.ElementAt(NextInt(numberPack, 1)).Key.renderer.material.color = new Color(1f, 1f, 1f, 0f);
-		
-		
-		cubesPos.ElementAt(PrevInt(numberPack, 1)).Key.renderer.material.color = new Color(1f, 1f, 1f, 0f);
-		
-		cubesPos.ElementAt(PrevInt(numberPack, 2)).Key.renderer.material.color = new Color(1f, 1f, 1f, 0.5f);
-		
-		cubesPos.ElementAt(PrevInt(numberPack, 3)).Key.renderer.material.color = new Color(1f, 1f, 1f, 1f);
-		
-		cubesPos.ElementAt(PrevInt(numberPack, 4)).Key.renderer.material.color = new Color(1f, 1f, 1f, 0.5f);
-	}
 	
 	int NextInt(int i, int recurs){
 		var res = 0;
@@ -263,6 +265,28 @@ public class PackZone : MonoBehaviour {
 		}
 	}
 	
+	
+	
+	public void popin()
+	{
+		for(int i=0; i < packs.Count; i++)
+		{
+			packs.ElementAt(i).Key.transform.position = Vector3.Lerp(packs.ElementAt(i).Key.transform.position, new Vector3(packs.ElementAt(i).Key.transform.position.x, posYModule.x, packs.ElementAt(i).Key.transform.position.z), Time.deltaTime/speedPop);
+		}
+	
+	}
+	
+	
+	public void popout()
+	{
+		for(int i=0; i < packs.Count; i++)
+		{
+			packs.ElementAt(i).Key.transform.position = Vector3.Lerp(packs.ElementAt(i).Key.transform.position, new Vector3(packs.ElementAt(i).Key.transform.position.x, posYModule.y, packs.ElementAt(i).Key.transform.position.z), Time.deltaTime/speedPop);
+		}
+	
+	}
+	
+	/*
 	void activePack(string s){
 		foreach(var el in songCubePack){
 			if(el.Value == s && el.Key.transform.position.y > - 3f*numberToDisplay){
@@ -295,7 +319,7 @@ public class PackZone : MonoBehaviour {
 				el.Key.SetActiveRecursively(false);
 			}
 		}
-	}
+	}*/
 	
 	
 }
